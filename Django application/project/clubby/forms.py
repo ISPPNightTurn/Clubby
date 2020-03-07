31060d3e1fc602308dadc6a5ec5db296a1896705
digitalvodka95 <digitalvodka95@gmail.com>
2020-03-07 20:21:54 +0100

    changed name and surname field to mandatory on register form

--- Django application/project/clubby/forms.py
+++ Django application/project/clubby/forms.py
@@ -33,8 +33,8 @@ class ClubModelForm(ModelForm):
         return data
 
 class SignupForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    first_name = forms.CharField(max_length=30, required=True )
+    last_name = forms.CharField(max_length=30, required=True )
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
 
     def clean(self):
