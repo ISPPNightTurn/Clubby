e6b1f9a048ee1c035c5fe5838e3aee5e263e701d
digitalvodka95 <digitalvodka95@gmail.com>
2020-03-10 19:05:30 +0100

    implemented permisions for club edit and delete, needs to be expanded to the rest of the models

--- Django application/project/clubby/views.py
+++ Django application/project/clubby/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User, Group
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import login_required, permission_required
 
+from django.core.exceptions import PermissionDenied
+
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 
@@ -195,12 +197,31 @@ class ClubUpdate(PermissionRequiredMixin,UpdateView):
     template_name = 'clubby/club/club_form.html'
     fields = ['name', 'address', 'max_capacity', 'NIF']
 
+    def form_valid(self, form):  
+        obj = form.save(commit=False)
+        self.object = obj # this is neccesary as the url is pulled from self.object.
+        if(obj.owner == self.request.user):
+            obj.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            raise PermissionDenied("You don't own that >:(")
+
+
 
 class ClubDelete(PermissionRequiredMixin,DeleteView):
     permission_required = 'clubby.is_owner'
     model = Club
     template_name = 'clubby/club/club_confirm_delete.html'
     success_url = reverse_lazy('clubs')
+
+    def delete(self, request, *args, **kwargs): #to check for permissions we override the default delete method
+        self.object = self.get_object()
+        can_delete = self.object.owner == self.request.user
+
+        if can_delete:
+            return super(ClubDeleteView, self).delete(request, *args, **kwargs)
+        else:
+            raise PermissionDenied("You don't own that >:(")
 
 #################
 #    PRODUCT    #
@@ -219,7 +240,7 @@ class ProductCreateView(PermissionRequiredMixin,CreateView):
         obj.owner = self.request.user
         self.object = obj # this is neccesary as the url is pulled from self.object.
         obj.save()
-        return HttpResponseRedirect('/clubby/myproducts')
+        return HttpResponseRedirect(reverse('my-products'))
 
 class ProductsByClubListView(LoginRequiredMixin, generic.ListView):
         """Generic class-based view listing events the user has participated, or is going to participate in."""
@@ -290,9 +311,10 @@ class EventCreateView(PermissionRequiredMixin,CreateView):
     def form_valid(self, form):  
         obj = form.save(commit=False)
         obj.owner = self.request.user
+        obj.club = obj.owner.club
         self.object = obj # this is neccesary as the url is pulled from self.object.
         obj.save()
-        return HttpResponseRedirect('/clubby/myevents')
+        return HttpResponseRedirect(reverse('my-events'))
 
 ##########################
 #    EXAMPLES (POLLS)    #
