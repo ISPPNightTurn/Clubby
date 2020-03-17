702cea0c9f73ea59d08f21bcc38b15c91b1b4277
digitalvodka95 <digitalvodka95@gmail.com>
2020-03-17 18:20:25 +0100

    changed project structure but now for real

--- Django application/project/clubby/views/__init__.py
+++
@@ -1,5 +0,0 @@
-#this file tells python to treat this folder as a package we need to include the views definition here
-from .views import *
-from .views_miguel import *
-from .views_jose import *
-from .views_javi import *
