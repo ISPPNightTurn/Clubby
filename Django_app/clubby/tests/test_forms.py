dda488ff1259d187f7c2918d91a0d9a7c36bce72
Raimundo <raimundokarate98@gmail.com>
2020-03-17 17:58:14 +0100

    Test to deploy using Travis

--- Django_app/clubby/tests/test_forms.py
+++ Django_app/clubby/tests/test_forms.py
@@ -18,20 +18,20 @@ from clubby.forms import EventModelForm
 
 #This type of tests are unit tests but you can also test views:
 #This is an example test
-class YourTestClass(TestCase):
-    def setUp(self):
-        # Setup run before every test method.
-        pass
+# class YourTestClass(TestCase):
+#     def setUp(self):
+#         # Setup run before every test method.
+#         pass
 
-    def tearDown(self):
-        # Clean up run after every test method.
-        pass
+#     def tearDown(self):
+#         # Clean up run after every test method.
+#         pass
 
-    def test_something_that_will_pass(self):
-        self.assertFalse(False)
+#     def test_something_that_will_pass(self):
+#         self.assertFalse(False)
 
-    def test_something_that_will_fail(self):
-        self.assertTrue(False)
+#     def test_something_that_will_fail(self):
+#         self.assertTrue(False)
 
 
 class SignUpFormTest(TestCase):
