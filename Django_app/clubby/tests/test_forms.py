e0b73acd63e9414c21d1dc5a5e33d647d9ef7c05
Javier <jvrgarciafernandez@gmail.com>
2020-03-28 23:33:52 +0100

    Tests de ejemplo

--- Django_app/clubby/tests/test_forms.py
+++ Django_app/clubby/tests/test_forms.py
@@ -3,9 +3,7 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 
-from clubby.forms import SignupForm
-from clubby.forms import ProductModelForm
-from clubby.forms import EventModelForm
+from clubby.forms import ClubModelForm
 
 
 
@@ -16,118 +14,60 @@ from clubby.forms import EventModelForm
 #    CLUBBY TESTS     #
 #######################
 
-#This type of tests are unit tests but you can also test views:
-#This is an example test
-class YourTestClass(TestCase):
-    def setUp(self):
-        # Setup run before every test method.
-        pass
+class ClubModelFormTests(TestCase):
+    def test_form_complete(self):
+        form_data = {'name': 'name', 'address': 'address', 'max_capacity': 10, 'NIF': '12345678x', 'picture': 'https://picture.com'}
+        form = ClubModelForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
-    def tearDown(self):
-        # Clean up run after every test method.
-        pass
+    def test_form(self):
+        form_data = {'name': 'name', 'address': 'address', 'max_capacity': 10, 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
-    def test_something_that_will_pass(self):
-        self.assertFalse(False)
+    def test_form_name_too_big(self):
+        form_data = {'name': 'namenamenamenamenamenamenamenamenamenamenamenamenamenamenamenamenamename',
+            'address': 'address', 'max_capacity': 10, 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
-    def test_something_that_will_fail(self):
-        self.assertTrue(False)
+    def test_form_name_null(self):
+        form_data = {'address': 'address', 'max_capacity': 10, 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
+    def test_form_address_too_big(self):
+        form_data = {'name': 'name','address': 'addressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddressaddress',
+        'max_capacity': 10, 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
-class SignUpFormTest(TestCase):
-    def test_sign_up_form_first_name_char_field_label(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['first_name'].label == None or form.fields['first_name'].label == 'first_name')
+    def test_form_address_null(self):
+        form_data = {'name': 'name', 'max_capacity': 10, 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
-    def test_sign_up_form_first_name_char_field_help_text(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['first_name'].help_text, 'Required. 30 character max')
+    def test_form_max_capacity_neg(self):
+        form_data = {'name': 'name', 'address': 'address', 'max_capacity': -1, 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
+    def test_form_max_capacity_null(self):
+        form_data = {'name': 'name','address': 'address', 'NIF': '12345678x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
-   
-    def test_sign_up_form_email_email_field_label(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['email'].label == None or form.fields['email'].label == 'email')
+    def test_form_NIF_7_numbers(self):
+        form_data = {'name': 'name', 'address': 'address', 'max_capacity': 10, 'NIF': '1234567x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
-    def test_sign_up_form_email_email_field_help_text(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['email'].help_text, 'Required. Inform a valid email address.')
+    def test_form_NIF_9_numbers(self):
+        form_data = {'name': 'name', 'address': 'address', 'max_capacity': 10, 'NIF': '123456789x'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
 
-
-
-    def test_sign_up_form_bith_date_date_field_label(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['birth_date'].label == None or form.fields['birth_date'].label == 'birth_date')
-
-    def test_sign_up_form_birth_date_date_field_help_text(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['birth_date'].help_text, 'Required, your birthday, format: YYYY-MM-DD')
-
-
-
-    def test_sign_up_form_bio_char_field_label(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['bio'].label == None or form.fields['bio'].label == 'bio')
-
-    def test_sign_up_form_bio_char_field_help_text(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['bio'].help_text, 'Optional, tell us something about you.')
-
-
-
-
-    def test_sign_up_form_location_char_field_label(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['location'].label == None or form.fields['location'].label == 'location')
-
-    def test_sign_up_form_location_char_field_help_text(self):
-        form = SignupForm()
-        self.assertTrue(form.fields['bio'].help_text, 'Optional, where are you form?.')
-
-
-    
-
-
-class ProductModelFormTest(TestCase):
-
-    def test_product_model_form_name_char_field_label(self):
-        form = ProductModelForm()
-        self.assertTrue(form.fields['name'].label == None or form.fields['location'].label == 'location')
-
-    def test_product_model_form_name_char_field_help_text(self):
-        form = ProductModelForm()
-        self.assertTrue(form.fields['name'].help_text, 'Required. 50 character max')
-
-
-
-
-    def test_product_model_form_price_decimal_field_label(self):
-        form = ProductModelForm()
-        self.assertTrue(form.fields['price'].label == None or form.fields['price'].label == 'price')
-
-
-    def test_product_model_form_price_decimal_field_help_text(self):
-        form = ProductModelForm()
-        self.assertTrue(form.fields['price'].help_text, 'Required. 5 digits max')
-
-
-
-
-class EventModelFormTest(TestCase):
-
-    def test_event_model_form_name_char_field_label(self):
-        form = EventModelForm()
-        self.assertTrue(form.fields['name'].label == None or form.fields['name'].label == 'name')
-
-    def test_event_model_form_name_char_field_help_text(self):
-        form = EventModelForm()
-        self.assertTrue(form.fields['name'].help_text, 'Required. 50 character max')   
-
-
-
-    def test_event_model_form_start_date_date_field_label(self):
-        form = EventModelForm()
-        self.assertTrue(form.fields['start_date'].label == None or form.fields['start_date'].label == 'price')
-
-
-   
+    def test_form_NIF_no_letter(self):
+        form_data = {'name': 'name', 'address': 'address', 'max_capacity': 10, 'NIF': '12345678'}
+        form = ClubModelForm(data=form_data)
+        self.assertFalse(form.is_valid())
